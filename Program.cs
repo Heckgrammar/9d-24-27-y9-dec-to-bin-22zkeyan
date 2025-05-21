6c3a805262f881988062a6c3fcd458df5c6c4c14
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-05-21 12:18:01 +0100

    idk

--- Program.cs
+++ Program.cs
@@ -11,24 +11,34 @@ namespace Y9_DEC_TO_BIN_SKELETON
         static void Main(string[] args)
         {
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
+            double myInt = 125.00; //just for testing single stepping
             string myString = "12"; //watch me being cast from string to int
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
             //MAIN CHALLENGE: WRITE A PROGRAM TO CONVERT ANY INTEGER TO ITS EQUIVALENT BINARY NUMBER
+            string binaryNum = "";
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                num = num / 2;
+                if (num % 2 == 0)
+                {
+                    binaryNum
+                }
+            }
             //WRITE A CALL TO YOUR NUMBER CONVERSION FUNCTION HERE
-            
-            
+
+
         }
 
         // LET'S USE THE 'STRUCTURED APPROACH' TO PROGRAMMING...A QUICK INTRODUCTION TO SUBBROUTINES(functions and procedures)...a.k.a. 'methods' in C#
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
+        /*static int numberConversion(int number, int numberbase)
         {
             //CODE GOES HERE
-
-            return result; //REMOVE THE RED LINE!
-        }
+            
+            //return result; //REMOVE THE RED LINE!
+        }*/
     }
 }

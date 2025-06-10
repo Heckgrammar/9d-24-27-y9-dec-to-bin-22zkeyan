5e60a2ace8c11e4d6770582ba511ae0f91874f86
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-06-10 12:19:59 +0100

    finished number conv

--- Program.cs
+++ Program.cs
@@ -16,29 +16,55 @@ namespace Y9_DEC_TO_BIN_SKELETON
             int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
 
             //MAIN CHALLENGE: WRITE A PROGRAM TO CONVERT ANY INTEGER TO ITS EQUIVALENT BINARY NUMBER
-            string binaryNum = "";
-            int num = Convert.ToInt32(Console.ReadLine());
-            while (num > 0)
-            {
-                num = num / 2;
-                if (num % 2 == 0)
-                {
-                    binaryNum
-                }
-            }
+
             //WRITE A CALL TO YOUR NUMBER CONVERSION FUNCTION HERE
 
+            NumberConv();
+        }
 
+        static void NumberConv()
+        {
+            Console.Write("number: ");
+            string binaryNum = "";
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                
+                if (num % 2 == 0)
+                {
+                    binaryNum = "0" + binaryNum;
+                }
+                else
+                {
+                    binaryNum = "1" + binaryNum;
+                }
+                num = num / 2;
+            }
+            Console.WriteLine(binaryNum);
         }
 
         // LET'S USE THE 'STRUCTURED APPROACH' TO PROGRAMMING...A QUICK INTRODUCTION TO SUBBROUTINES(functions and procedures)...a.k.a. 'methods' in C#
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
-        /*static int numberConversion(int number, int numberbase)
+        static int numberConversion(int number, int numberbase)
         {
             //CODE GOES HERE
-            
+            string binaryNum = "";
+            int num = number;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    return Convert.ToInt32("0" + binaryNum);
+                }
+                else
+                {
+                    return Convert.ToInt32("1" + binaryNum);
+                }
+
+            }
+            return Convert.ToInt32(binaryNum);
             //return result; //REMOVE THE RED LINE!
-        }*/
+        }
     }
 }
